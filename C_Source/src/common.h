97489ecd781a4408bf2223416f3e46efd054bdc6
Gabe Ambrosio <ambrosiogabe@gmail.com>
2019-10-09 14:41:56 -0500

    Added c scanner-- UNTESTED_CURRENTLY

--- C_Source/src/common.h
+++ C_Source/src/common.h
@@ -2,6 +2,7 @@
 
 #define nullptr NULL
 #define DEBUG_TRACE_EXECUTION
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdbool.h>
 #include <stddef.h>
--- C_Source/src/main.c
+++ C_Source/src/main.c
@@ -1,28 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
-#include <stdlib.h>
+
+static void repl() {
+	char line[1024];
+	for (;;) {
+		printf(">> ");
+
+		if (!fgets(line, sizeof(line), stdin)) {
+			printf("\n");
+			break;
+		}
+
+		interpret(line);
+	}
+}
+
+static char* readFile(const char* path) {
+	FILE* file = fopen(path, "rb");
+	if (file == nullptr) {
+		fprintf(stderr, "Could not open file \"%s\".\n", path);
+		exit(74);
+	}
+
+	fseek(file, 0L, SEEK_END);
+	size_t fileSize = ftell(file);
+	rewind(file);
+
+	char* buffer = (char*)malloc(fileSize + 1);
+	if (buffer == nullptr) {
+		fprintf(stderr, "Not enough memory to read\"%s\".\n", path);
+		exit(74);
+	}
+
+	size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+	if (bytesRead < fileSize) {
+		fprintf(stderr, "Could not read file \"%s\".\n", path);
+		exit(74);
+	}
+
+	buffer[bytesRead] = '\0';
+	fclose(file);
+	return buffer;
+}
+
+static void runFile(const char* path) {
+	char* source = readFile(path);
+	InterpretResult result = interpret(source);
+	free(source);
+
+	if (result == INTERPRET_COMPILE_ERROR) exit(65);
+	if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
 
 int main(int argc, const char* argv[]) {
 	initVM();
 
-	Chunk chunk;
-	initChunk(&chunk);
-
-	writeConstant(&chunk, 1.2, 123);
-	writeConstant(&chunk, 3.4, 123);
-	writeChunk(&chunk, OP_ADD, 123);
-
-	writeConstant(&chunk, 5.6, 123);
-	writeChunk(&chunk, OP_DIVIDE, 123);
-
-	writeChunk(&chunk, OP_RETURN, 200);
+	if (argc == 1) {
+		repl();
+	}
+	else if (argc == 2) {
+		runFile(argv[1]);
+	}
+	else {
+		fprintf(stderr, "Usage: dymond [path]\n");
+		exit(64);
+	}
 	
-	disassembleChunk(&chunk, "Test chunk");
-	interpret(&chunk);
 	freeVM();
-	freeChunk(&chunk);
 	system("Pause");
 	return 0;
 }
